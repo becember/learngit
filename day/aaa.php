09a7749d04d30985b5ae9d0c42272ddeac8e9ce8
王江飞 <542442696@qq.com>
2019-03-28 08:51:41 +0800

    注释添加

--- day/aaa.php
+++ day/aaa.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * [FindNumbersWithSum 在数组中查找两个数，使得他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的]
+ * @param [array] $array [一个递增排序的数组]
+ * @param [int] $sum   [一个数字S]
+ */
 function FindNumbersWithSum($array,$sum)
 {
     $lenth = count($array);
