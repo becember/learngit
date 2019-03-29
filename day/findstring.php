6e0be9611a8849ccb3b309283f80e4b037e3577e
王江飞 <542442696@qq.com>
2019-03-29 08:55:42 +0800

    十五单元技能

---
+++ day/findstring.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * [FirstNotRepeatingChar 寻找字符串中第一个只出现一次的字符]
+ * @param [string] $str [被寻找的字符串]
+ */
+function FirstNotRepeatingChar($str)
+{
+    $lenth = strlen($str);
+    if ($lenth>=0 && $lenth<=10000) {
+        for ($i=1; $i <= $lenth ; $i++) { 
+                  
+        }
+    }
+    return "i";
+}
+$str = "asidfhysdfuygsudfgsubuysadgufyasdfbuaysygdfuysadufvsdvuysabdufygas";
+echo FirstNotRepeatingChar($str);
